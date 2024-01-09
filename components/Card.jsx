298f8c1a68ce72e76051ab5aae8733c95d4cbab4
yjb23tech <yjb23tech@gmail.com>
2024-01-09 19:43:48 +0000

    Anti Tech Evangelist

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,7 +1,26 @@
+import position_icon from "../public/images/position_icon.png"
+import scoring_icon from "../public/images/scoring_icon.png"
+import team_icon from "../public/images/team_icon.png"
+
 const Card = (props) => {
     return (
         <div>
             <img src={`images/${props.nba_champ_img}`} className="nba-champ-img"/>
+            <div>
+                <h1>{props.nba_champ_player_name}</h1>
+                <div>
+                    <img src={position_icon} />
+                    <p>{props.nba_champ_position}</p>
+                </div>
+                <div>
+                    <img src={scoring_icon} />
+                    <p>{props.nba_champ_season_average}</p>
+                </div>
+                <div>
+                    <img src={team_icon} />
+                    <p>{props.nba_champ_team}</p>
+                </div>
+            </div>
         </div>
     )
 }
