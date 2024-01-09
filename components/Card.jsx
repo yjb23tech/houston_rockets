656fca6d9aca0c7cb8c1f859be48fcc3c47fda9b
yjb23tech <yjb23tech@gmail.com>
2024-01-09 20:25:40 +0000

    You did well

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,19 +4,19 @@ import team_icon from "../public/images/team_icon.png"
 
 const Card = (props) => {
     return (
-        <div>
-            <img src={`images/${props.nba_champ_img}`} className="nba-champ-img"/>
-            <div>
+        <div className="nba-champ-card util-box-red">
+            <img src={`images/${props.nba_champ_img}`} className="nba-champ-img util-box-blue"/>
+            <div className="nba-champ-content util-box-orange">
                 <h1>{props.nba_champ_player_name}</h1>
-                <div>
+                <div className="info-group util-box-green">
                     <img src={position_icon} />
                     <p>{props.nba_champ_position}</p>
                 </div>
-                <div>
+                <div className="info-group util-box-green">
                     <img src={scoring_icon} />
                     <p>{props.nba_champ_season_average}</p>
                 </div>
-                <div>
+                <div className="info-group util-box-green">
                     <img src={team_icon} />
                     <p>{props.nba_champ_team}</p>
                 </div>
